749e2e0b3b1f9ffa455508c838909d3b5c93f823
solarbrisngr <59808313+solarbrisngr@users.noreply.github.com>
2020-03-30 21:17:11 -0700

    Add files via upload

---
+++ Decision Tree/BehaviorClient.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorClient : MonoBehaviour
+{
+    bool executingBehavior = false;
+    tasks myCurrentTask;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (!executingBehavior)
+            {
+                myCurrentTask = getCurrentTask();
+                executingBehavior = true;
+                myCurrentTask.run();
+            }
+        }
+    }
+
+    public tasks getCurrentTask()
+    {
+        List<tasks> taskList = new List<tasks>();
+
+        tasks openDoor = new OpenDoor();
+        tasks bargeIn = new BargeDoor();
+
+        taskList.Add(openDoor);
+        taskList.Add(bargeIn);
+
+        sequence bargeClosedDoor = new sequence(taskList);
+
+        return bargeClosedDoor;
+    }
+}
+
+
---
+++ Decision Tree/tasks.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tasks
+{
+    public virtual bool run() { return false; }
+}
+
+public class selector : tasks
+{
+    List<tasks> children;
+    tasks currentTask;
+    int currentTaskIndex = 0;
+
+    public selector(List<tasks> taskList)
+    {
+        children = taskList;
+    }
+
+    public override bool run()
+    {
+        foreach (tasks i in children)
+        {
+            if (i.run())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+public class sequence : tasks
+{
+    List<tasks> children;
+    tasks currentTask;
+    int currentTaskIndex = 0;
+
+    public sequence(List<tasks> taskList)
+    {
+        children = taskList;
+    }
+
+    public override bool run()
+    {
+        foreach (tasks i in children)
+        {
+            if (!i.run())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+public class OpenDoor : tasks
+{
+
+    public override bool run()
+    {
+        Debug.Log("The door is now open!");
+        return true;
+    }
+}
+
+public class BargeDoor : tasks
+{
+    public override bool run()
+    {
+        Debug.Log("You hit the shoulder on the door.");
+        return true;
+    }
+}
+
+
